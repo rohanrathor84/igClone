c187a96858e04c5fb45e980c5f6da94c52d060fe
Rohan kumar <rohan.kumar@firstcry.com>
2022-06-11 18:29:28 +0530

    changes

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,13 +2,11 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
-import App from '../App';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import Navigations from '../src/Navigations';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('renders correctly', () => {
+  const tree = renderer.create(<Navigations />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
